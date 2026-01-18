89da35499b5ab2eb82c05319c7c481b2be368e5a
Rudraharan Nivaethan <nivaethanrudraharan@gmail.com>
2026-01-18 16:20:42 +0530

    TextInput page created

--- FC222008_SoftwareTestingAndValidation_Assignment_02/Pages/TextInput/TextInputPage.cs
+++ FC222008_SoftwareTestingAndValidation_Assignment_02/Pages/TextInput/TextInputPage.cs
@@ -12,5 +12,31 @@ namespace FC222008_SoftwareTestingAndValidation_Assignment_02.Pages.TextInput
     internal class TextInputPage : BasePage
     {
         public TextInputPage(IWebDriver driver) : base(driver) { }
+
+        // Element Locaters
+        private IWebElement txtButtonName => FindElement(By.CssSelector("input#newButtonName"));
+        private IWebElement btnUpdate => FindElement(By.CssSelector("button#updatingButton"));
+
+        //-------------------------------------------------//
+        //                    Actions                      //
+        //-------------------------------------------------//
+        // 1. Helper method to set new button name
+        public void SetButtonName(string name)
+        {
+            txtButtonName.Clear();
+            txtButtonName.SendKeys(name);
+        }
+
+        // 2. Helper method to click button
+        public void ClickUpdateButton() 
+        {
+            btnUpdate.Click();
+        }
+
+        // 3. Helper Method to get Button Text
+        public string GetButtonTxt()
+        {
+            return btnUpdate.Text;
+        }
     }
 }
