f61e1f26a9c7f3a86336860992319cbb881923f8
Rudraharan Nivaethan <nivaethanrudraharan@gmail.com>
2026-01-18 15:27:58 +0530

    Created folder structure and created base page class and test driver class

---
+++ FC222008_SoftwareTestingAndValidation_Assignment_02/Framework/BasePage.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FC222008_SoftwareTestingAndValidation_Assignment_02.Framework
+{
+    // Parent class for all page objects in the framework
+    internal class BasePage
+    {
+        // Holds the reference to the WebDriver instance used by this page
+        protected IWebDriver _driver;
+
+        // Constructor to initialize the page with a WebDriver
+        // Any page class inheriting BasePage will have access to this driver
+        protected BasePage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        // Custom method to find an element with an explicit wait => by: locator (e.g., By.Id, By.XPath, and so on)
+        // timeoutSeconds: maximum wait time in seconds (default 30)
+        protected IWebElement FindElement(By by, int timeoutSeconds = 30)
+        {
+            // Waits until the element is found or the timeout expires
+            return new WebDriverWait(_driver, TimeSpan.FromSeconds(timeoutSeconds)).Until(d => d.FindElement(by));
+        }
+    }
+}
---
+++ FC222008_SoftwareTestingAndValidation_Assignment_02/Framework/TestDriver.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FC222008_SoftwareTestingAndValidation_Assignment_02.Framework
+{
+    internal class TestDriver
+    {
+        // Method that creates and returns a Chrome WebDriver instance
+        public static IWebDriver GetDriver()
+        {
+            // To configure browser behavior
+            ChromeOptions options = new ChromeOptions();
+
+            // Opens the Chrome browser in maximized mode
+            options.AddArgument("start-maximized");
+
+            // Creates and returns a new ChromeDriver using the given options
+            return new ChromeDriver(options);
+        }
+
+        // Generic method to read JSON data and convert it into any required object type: Future reference
+        public static T ReadJson<T>(string filePath)
+        {
+            // Reads the entire JSON file content as a string
+            string json = File.ReadAllText(filePath);
+
+            // Converts JSON string into an object of type T and returns it
+            return JsonConvert.DeserializeObject<T>(json)!;
+        }
+    }
+}
