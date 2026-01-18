6dbc4ec6adcd9ad3fbd798d433b255c11f806947
Rudraharan Nivaethan <nivaethanrudraharan@gmail.com>
2026-01-18 15:57:15 +0530

    Created Home page

---
+++ FC222008_SoftwareTestingAndValidation_Assignment_02/Pages/Home/HomePage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FC222008_SoftwareTestingAndValidation_Assignment_02.Framework;
+using FC222008_SoftwareTestingAndValidation_Assignment_02.Pages.TextInput;
+using OpenQA.Selenium;
+
+namespace FC222008_SoftwareTestingAndValidation_Assignment_02.Pages.Home
+{
+    internal class HomePage : BasePage
+    {
+        public HomePage(IWebDriver driver) : base(driver) { }
+
+        // Element Locaters
+        private IWebElement lnkTextInput => FindElement(By.CssSelector("a[href='/textinput']"));
+
+        // Method to navigate to Text Input page
+        public TextInputPage GoToTextInput()
+        {
+            lnkTextInput.Click();
+            return new TextInputPage(_driver);
+        }
+    }
+}
---
+++ FC222008_SoftwareTestingAndValidation_Assignment_02/Pages/TextInput/TextInputPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FC222008_SoftwareTestingAndValidation_Assignment_02.Framework;
+using OpenQA.Selenium;
+
+namespace FC222008_SoftwareTestingAndValidation_Assignment_02.Pages.TextInput
+{
+    internal class TextInputPage : BasePage
+    {
+        public TextInputPage(IWebDriver driver) : base(driver) { }
+    }
+}
